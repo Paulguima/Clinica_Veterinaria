f399da01ca5bf875b9ece1b2d0cf2a97e1d78dfb
Paulguima <paulo.jguimaraes03@gmail.com>
2025-12-01 23:21:28 -0300

    Criando Update para as tabelas

---
+++ 03_UpDate.sql
@@ -0,0 +1,34 @@
+--- UPDATE - Atualizações SQ----
+
+update cliente
+ set nome = 'João' 
+where id_cliente = 2;
+
+ 
+select * from cliente
+
+--ordenar
+
+select * from pet
+Order BY nome;
+
+select * from consulta
+Order BY data ASC;
+
+---quantas consultas cada veterianrio fez---
+
+select
+    v.nome AS veterinario,
+    COUNT(c.id_consulta) AS Total_consultas
+FROM consulta c
+JOIN veterinario v ON v.id_veterinario = c.id_veterinario
+order BY v.nome;
+
+
+
+
+
+
+
+
+
